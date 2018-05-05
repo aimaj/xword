2a224fdf8f63dbe597c8ce6d08886f62d3d59dcf
aimaj <aidan.majewski@gmail.com>
2018-05-05 11:18:15 +1000

    working Tile replication with array of vals

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import './App.css';
 import Tile from './Tile.js';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    const numbers = [1, 2, 3, 4, 5];
+    const listItems = numbers.map((number) => 
+      <Tile text="{number}"></Tile>);
+    this.state = {list : listItems};
+  }
+
   render() {
     return (
-      <Tile text="this text here"></Tile>
+      this.state.list
     );
   }
 }
