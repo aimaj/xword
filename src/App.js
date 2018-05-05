e663795822b08b344f02ed89500d153924eb96ed
aimaj <aidan.majewski@gmail.com>
2018-05-05 10:55:47 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Tile from './Tile.js';
+
+class App extends Component {
+  render() {
+    return (
+      <Tile text="this text here"></Tile>
+    );
+  }
+}
+
+
+export default App;
+
